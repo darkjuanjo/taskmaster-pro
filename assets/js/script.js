55fd1e5ea0a3dea171f3f7c319647c24078b25c0
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-07-22 16:02:03 -0700

    added drag-n-drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,7 +137,73 @@ $(".list-group").on("blur", "textarea", function() {
 
     // replace input with span element
     $(this).replaceWith(taskSpan);
-  })
+  });
+
+  $(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event) {
+      // console.log("activate", this);
+    },
+    deactivate: function(event) {
+      // console.log("deactivate", this);
+    },
+    over: function(event) {
+      // console.log("over", event.target);
+    },
+    out: function(event) {
+      // console.log("out", event.target);
+    },
+    update: function(event) {
+      // console.log("update", this);
+      // array to store the task data in
+      var tempArr = [];
+      // loop over current set of children in sortable list
+      $(this).children().each(function() {
+        var text = $(this)
+          .find("p")
+          .text()
+          .trim();
+
+        var date = $(this)
+          .find("span")
+          .text()
+          .trim();
+
+            // add task data to the temp array as an object
+          tempArr.push({
+            text: text,
+            date: date
+          });
+          // console.log(text, date);
+      });
+      // trim down list's ID to match object property
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+    }
+  });
+
+  $("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+      console.log("drop");
+      ui.draggable.remove();
+    },
+    over: function(event, ui) {
+      console.log("over");
+    },
+    out: function(event, ui) {
+      console.log("out");
+    }
+  });
 
 
 
@@ -153,7 +219,9 @@ $("#task-form-modal").on("shown.bs.modal", function() {
   $("#modalTaskDescription").trigger("focus");
 });
 
-// save button in modal was clicked
+// save 
+
+//button in modal was clicked
 $("#task-form-modal .btn-primary").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
